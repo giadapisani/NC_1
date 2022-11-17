234e32e3586a54a4c21620201fee623e0e3d08e7
giadapisani <114001878+giadapisani@users.noreply.github.com>
2022-11-17 16:54:55 +0100

    Add files via upload

---
+++ NC1/NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Giada Pisani on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
